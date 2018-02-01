b74ea0a343853189669ec0d281d04d01076070ca
Jack Wu <jackwu350@yahoo.com>
2018-01-31 16:11:39 -0800

    rendered artwork and artist info in album

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
 
-  const {album}  = props;
+  const {title,artist,thumbnail_image,image}  = album;
 
   return(
     <Card>
       <CardSection>
-        <Text> {album.title} </Text>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+          style={styles.thumbnailStyle}
+          source={ { uri: thumbnail_image} }
+          />
+        </View>
+
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}> {title} </Text>
+          <Text> {artist} </Text>
+        </View>
+
+      </CardSection>
+
+      <CardSection>
+        <Image
+        style={styles.imageStyle}
+        source={ { uri: image} }
+        />
+
       </CardSection>
     </Card>
   );
@@ -17,3 +36,33 @@ const AlbumDetail = (props) => {
 }
 
 export default AlbumDetail;
+
+
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+
+  imageStyle:{
+    height: 300,
+    flex: 1,
+    width: null,
+  },
+
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+
+  headerTextStyle:{
+    fontSize: 18,
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+}
